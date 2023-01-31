1c32b0f168ee40ad733f3211cf4f625ae09d7436
des-afari <afaridesmond@gmail.com>
2023-01-31 17:49:23 +0000

    task 103

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def magic_string(n=[]):
     # function objects can have attributes, here at global count of calls
-    if hasattr(magic_string, 'calls'):
-        magic_string.calls += 1
-    else:
-        magic_string.calls = 1
-
-    return ', '.join(['BestSchool'] * magic_string.calls) + '$'
+    n += ['BestSchool']
+    return (", ".join(n) + '$')
