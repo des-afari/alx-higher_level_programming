fadc5d574598dcdcb5b013ff35656220e568e4fc
des-afari <afaridesmond@gmail.com>
2023-02-07 18:27:55 +0000

    read a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def read_file(filename=""):
+def read_file(filename="", mode="r", encoding="utf-8"):
     with open(filename) as f:
         for line in f:
             print(line, end="")
