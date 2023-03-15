3b6db97b1dbdd812c6f5e4239d0ce7c683236248
des-afari <afaridesmond@gmail.com>
2023-03-15 22:11:06 +0000

    advanced tasks

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,4 +1,4 @@
---uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+-- uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
 SELECT DISTINCT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,4 +1,4 @@
---lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
 SELECT tv_genres.name, SUM(tv_show_ratings.rating) as rating_sum
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
