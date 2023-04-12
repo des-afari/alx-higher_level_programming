e119c84290f8a1da5893ec417ab9d22d5b9369ca
des-afari <afaridesmond@gmail.com>
2023-04-12 18:51:11 +0000

    second

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length < 4) {
+  console.log(0);
+} else {
+  args.splice(0, 2);
+  const intArgs = args.map(Number);
+  const maxElement = Math.max(...intArgs);
+  const index = intArgs.indexOf(maxElement);
+
+  if (index > -1) {
+    intArgs.splice(index, 1);
+  }
+  console.log(Math.max(...intArgs));
+}
