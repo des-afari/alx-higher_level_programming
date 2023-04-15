9fa08deb1be111ab50d30597f5801bbb983ec8da
des-afari <afaridesmond@gmail.com>
2023-04-15 21:39:17 +0000

    filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-""" Filter states based on argument """
+""" Lists all states from database hbtn_0e_0_usa by name """
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
-                           host="localhost", port=3306)
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name='{}' \
-    OREDER BY id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
+                .format(argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
         if row[1][:] == argv[4]:
             print(row)
-
     cur.close()
     conn.close()
