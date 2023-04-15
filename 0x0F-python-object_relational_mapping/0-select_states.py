a97776fbf2b586b123252ecf7a5de93aafc237dc
des-afari <afaridesmond@gmail.com>
2023-04-15 20:46:38 +0000

    select all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Select all states in a database """
+
+import MySQLdb
+from sys import argv
+
+
+conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cur.fetchall()
+
+for row in rows:
+    print(row)
+
+cur.close()
+db.close()
