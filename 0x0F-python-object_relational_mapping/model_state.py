27d9703b713a608ba08374fe1faa92fdc1357cd9
des-afari <afaridesmond@gmail.com>
2023-04-16 10:31:22 +0000

    declarative

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 """First state model"""
 
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
