5abb5c71596b0dd072f69e5ebca5385f92a9ed65
des-afari <afaridesmond@gmail.com>
2023-04-28 20:43:43 +0000

    urllib request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """Get Status
 """
+from urllib import request
 
-if __name__ == "__main__":
-    from urllib import request
-
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
-    # charset can be gained with response.headers.get_content_charset()
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+print('Body response:')
+print('\t- type: {}'.format(type(html)))
+print('\t- content: {}'.format(html))
+print('\t- utf8 content: {}'.format(html.decode('utf-8')))
