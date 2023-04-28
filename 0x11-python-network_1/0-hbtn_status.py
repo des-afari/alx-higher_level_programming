fdf02b491e35f1e04c8945ab30c4e2123ac7c948
des-afari <afaridesmond@gmail.com>
2023-04-28 20:39:28 +0000

    urllib request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
         html = response.read()
 
     print('Body response:')
-    print(f'\t- type: {type(html)}')
-    print(f'\t- content: {html}')
-    print(f'\t- utf8 content: {html.decode('utf-8')}')
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {html.decode('utf-8')}")
