3711c2001eec9c76e527d86561686fc875f6eec2
des-afari <afaridesmond@gmail.com>
2023-04-28 20:56:21 +0000

    urllib header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,9 @@
     Response header value #0
 """
 
-from urllib import request
-from sys import argv
+if __name__ == "__main__":
+    from urllib import request
+    from sys import argv
 
-with request.urlopen(argv[1]) as response:
-    print(response.getheader('X-Request-Id'))
+    with request.urlopen(argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
